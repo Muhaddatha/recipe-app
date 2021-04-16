3bb28e723ddcb21e6ec528db5846c5aad960bf33
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-04-15 22:40:29 -0400

    first fragment GUI and spinners

--- app/src/main/java/com/example/foodiefood/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/foodiefood/ui/main/MainFragment.kt
@@ -7,11 +7,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.foodiefood.MainActivity
 import com.example.foodiefood.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
+
+    //lateinit var adapterArray : ArrayAdapter<String>
 
     companion object {
         fun newInstance() = MainFragment()
@@ -27,16 +30,35 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
 
         searchButton.setOnClickListener {
             handleGetRecipe(it)
+        }
+
+
+
+        // setting up spinner options
+        ArrayAdapter.createFromResource(activity!!.applicationContext, R.array.dietOptions, android.R.layout.simple_spinner_item).also {
+            adapter->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+            dietSpinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(activity!!.applicationContext, R.array.cuisineOptions, android.R.layout.simple_spinner_item).also{
+            adapter->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+            cuisineSpinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(activity!!.applicationContext, R.array.mealTypeOptions, android.R.layout.simple_spinner_item).also{
+            adapter->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+            mealTypeSpinner.adapter = adapter
         }
     }
 
     private fun handleGetRecipe(view : View){
         Log.i("test", "inside handleGetRecipe function in main fragment")
-
         var url = "https://api.spoonacular.com/recipes/random?number=1&tags=korean&apiKey="
         (activity as MainActivity).apiCall(url, this.id)
 
